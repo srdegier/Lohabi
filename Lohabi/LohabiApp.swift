3f95812c7bbd812a4d5db62067f10948a0599a2e
Stefan de Gier <stefan_de_gier@outlook.com>
2023-12-27 13:35:30 +0100

    Initial Commit

---
+++ Lohabi/LohabiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LohabiApp.swift
+//  Lohabi
+//
+//  Created by Stefan de Gier on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct LohabiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
