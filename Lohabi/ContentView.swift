7d5ceadff79d57da0d7eef0f80ea618fc36e4c50
Stefan de Gier <stefan_de_gier@outlook.com>
2023-12-28 19:19:13 +0100

    LOH-13 Added footer with add button and Scrollview with demo content
    Test description - ignore this text "Test description"

--- Lohabi/ContentView.swift
+++ Lohabi/ContentView.swift
@@ -10,16 +10,46 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            VStack {
+            ZStack(alignment: .bottomLeading) {
+                ScrollView() {
+                    // Demo for Lohabi cards
+                    VStack(spacing: 24) {
+                        ForEach(0 ..< 6) { item in
+                            RoundedRectangle(cornerRadius: 12)
+                                .fill(Color.white)
+                                .frame(height: 130)
+                                .shadow(radius: 0.5)
+                        }
+                    }
+                    .padding(24)
+                }
+                .padding(.bottom, 70)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 80)
+                        .fill(Color.white)
+                        .frame(maxWidth: .infinity, maxHeight: 80)
+                        .shadow(radius: 0.5)
+                    Button(action: {
+                
+                    }) {
+                        Image(systemName: "plus")
+                            .font(.system(size: 35))
+                            .foregroundColor(.white)
+                            .frame(width: 70, height: 70)
+                            .background(Color.blue)
+                            .clipShape(Circle())
+                    }
+                    .offset(y: -30)
 
+                }
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color(UIColor.secondarySystemBackground))
             .navigationTitle("Lohabi")
             .toolbarBackground(
                 BackgroundStyle(),
                 for: .navigationBar)
             .toolbarBackground(.visible, for: .navigationBar)
+            .background(Color(UIColor.secondarySystemBackground))
+            .ignoresSafeArea(edges: [.bottom])
         }
     }
 }
