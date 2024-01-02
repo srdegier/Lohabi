29b717f557e622f00b6335ac2e888d378f4e7e29
Stefan de Gier <stefan_de_gier@outlook.com>
2024-01-03 00:17:04 +0100

    LOH-7 Added LohabiSecondaryButton component

--- Lohabi/Components/LohabiSecondaryButton.swift
+++ Lohabi/Components/LohabiSecondaryButton.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct LohabiSecondaryButton: View {
+    var text: LocalizedStringKey
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: action, label: {
+            Text(text)
+                .fontWeight(.bold)
+                .foregroundColor(.blue)
+                .padding(16)
+                .frame(maxWidth: .infinity)
+                .cornerRadius(16)
+        })
+        .padding(.horizontal, 24)
+    }
+}
