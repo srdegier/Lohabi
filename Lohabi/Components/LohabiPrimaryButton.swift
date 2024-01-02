467c9705628989dc7aa37ac9e0a5726770ff9044
Stefan de Gier <stefan_de_gier@outlook.com>
2024-01-02 22:58:35 +0100

    LOH-8 Added LohabiPrimaryButton

---
+++ Lohabi/Components/LohabiPrimaryButton.swift
@@ -0,0 +1,28 @@
+//
+//  LohabiPrimaryButton.swift
+//  Lohabi
+//
+//  Created by Stefan de Gier on 02/01/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct LohabiPrimaryButton: View {
+    var text: LocalizedStringKey
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: action, label: {
+            Text(text)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .padding(16)
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .cornerRadius(16)
+        })
+        .padding(.horizontal, 24)
+        .padding(.vertical)
+    }
+}
