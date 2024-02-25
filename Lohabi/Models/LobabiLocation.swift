f7c76c652680243706fe109a294f6a36b892cd78
Stefan de Gier <stefan_de_gier@outlook.com>
2024-02-25 22:30:50 +0100

    LOH-19 Added search location

---
+++ Lohabi/Models/LobabiLocation.swift
@@ -0,0 +1,15 @@
+//
+//  LobabiLocation.swift
+//  Lohabi
+//
+//  Created by Stefan de Gier on 25/02/2024.
+//
+
+import Foundation
+import CoreLocation
+
+struct LohabiLocation {
+    let name: String
+    let locationInfo: [LocationInfo]
+    // further setup configs
+}
--- Lohabi/Views/AddLohabi/AddLohabiView.swift
+++ Lohabi/Views/AddLohabi/AddLohabiView.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct AddLohabiView: View {
+    @Environment(LocationManager.self) var locationManager
+    @State private var searchText = ""
+    @State private var selectedLocation: LocationInfo?
+    @State private var locations: [LocationInfo] = []
+    
     var body: some View {
-        Text("Add Lohabi")
+        LohabiNavigationStack {
+            ScrollView {
+                VStack {
+                    ForEach(locationManager.nearestLocations) { location in
+                        LocationRow(location: location, isSelected: selectedLocation?.id == location.id) {
+                            self.selectedLocation = location
+                        }
+                    }
+                }
+                .padding(.vertical, 24)
+            }
+            .padding()
+            .navigationTitle("Add Lohabi")
+            .navigationBarTitleDisplayMode(.inline)
+            .searchable(text: $searchText)
+            .onChange(of: searchText) {
+                self.locationManager.updateSearchResults(searchText: searchText)
+            }
+        }
+        .overlay {
+            if locationManager.nearestLocations.isEmpty {
+                ContentUnavailableView {
+                    Label(LocalizedStringKey("Search an address"), systemImage: "location.magnifyingglass")
+                } description: {
+                    Text(LocalizedStringKey("Enter the address where you want the measurement to take place."))
+                }
+            }
+        }
     }
 }
 
 #Preview {
     AddLohabiView()
+        .environment(LocationManager())
 }
